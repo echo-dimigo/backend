137d254ce21a9fe5f267c697081479a85b2affd0
uhmtoto <uhmtotow@gmail.com>
2019-10-13 09:33:08 +0900

    feat: designate comment's target post in model

--- src/models/comment.js
+++ src/models/comment.js
@@ -18,6 +18,10 @@ const commentModel = new Schema({
     type: String,
     enum: ['normal', 'deleted', 'hided'], // deleted: 사용자가 삭제, hided: 관리자가 숨김 처리
     default: 'normal'
+  },
+  for: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
   }
 })
 
