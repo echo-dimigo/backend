5a1da184f61e0c4552103dbdb728019f1a4abfc0
uhmtoto <uhmtotow@gmail.com>
2019-10-16 08:57:23 +0900

    refactor: use own resource instead of mongoose's method

--- src/routes/comment/index.js
+++ src/routes/comment/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { needAuthorization } from '@/resources/middlewares'
 import { check } from 'express-validator'
-import mongoose from 'mongoose'
+import isObjectId from '@/resources/isObjectId'
 import controllers from './controllers'
 
 const router = Router()
@@ -10,18 +10,15 @@ router.get('/:userId', (req, res, next) => {})
 
 router.post('/', [
   check('content').isString().not().isEmpty(),
-  check('target').custom(value =>
-    mongoose.Types.ObjectId.isValid(value))
+  check('target').custom(isObjectId)
 ], needAuthorization, controllers.CreateComment)
 
 router.delete('/:commentId', [
-  check('commentId').custom(value =>
-    mongoose.Types.ObjectId.isValid(value))
+  check('commentId').custom(isObjectId)
 ], needAuthorization, controllers.DeleteComment)
 
 router.put('/:commentId', [
-  check('commentId').custom(value =>
-    mongoose.Types.ObjectId.isValid(value)),
+  check('commentId').custom(isObjectId),
   check('content').isString().not().isEmpty()
 ], needAuthorization, controllers.EditComment)
 
--- src/routes/subscription/index.js
+++ src/routes/subscription/index.js
@@ -2,18 +2,16 @@ import { Router } from 'express'
 import { check } from 'express-validator'
 import controllers from './controllers'
 import { needAuthorization } from '@/resources/middlewares'
-import mongoose from 'mongoose'
+import isObjectId from '@/resources/isObjectId'
 
 const router = Router()
 
 router.post('/:tagId', [
-  check('tagId').custom(value =>
-    mongoose.Types.ObjectId.isValid(value))
+  check('tagId').custom(isObjectId)
 ], needAuthorization, controllers.CreateSubscription)
 
 router.delete('/:subscriptionId', [
-  check('subscriptionId').custom(value =>
-    mongoose.Types.ObjectId.isValid(value))
+  check('subscriptionId').custom(isObjectId)
 ], needAuthorization, controllers.CancelSubscription)
 
 export default router
