e7b3546158c9fb215801c50a1fd3ec400edea39f
uhmtoto <uhmtotow@gmail.com>
2019-10-16 12:40:39 +0900

    feat: add check validation result middleware

--- src/resources/middlewares/index.js
+++ src/resources/middlewares/index.js
@@ -1,7 +1,9 @@
 import attachUserInfo from './attachUserInfo'
 import needAuthorization from './needAuthorization'
+import checkValidation from './checkValidation'
 
 export {
   attachUserInfo,
-  needAuthorization
+  needAuthorization,
+  checkValidation
 }
